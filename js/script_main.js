bdfc26b2dbc3fe749bd6e0c4109911bea3e7b5af
jeonseunghyeon <klzxklzx123@naver.com>
2022-04-06 00:27:55 +0900

    애니메이션 기능 구현

--- js/script_main.js
+++ js/script_main.js
@@ -1,10 +1,14 @@
+//0. a클릭시 위로 튕기는 이벤트 제거
+$(document).on('click', 'a[href="#"]',function(e) {
+    e.preventDefault();
+}) ;
 //1. approach영역
 $(window).on('scroll resize', function() {
     var scrollPos = 0;
     scrollPos = $(document).scrollTop();
     fix();
-
-
+    fixHeader();
+    fixList();
     function fix() {
         if(scrollPos > 1250) {
             $('.fix .text').addClass('on');
@@ -12,8 +16,45 @@ $(window).on('scroll resize', function() {
             $('.fix .text').removeClass('on');
         }
 
-        if(scrollPos > 2700) { $('.fix .text').removeClass('on')}
+        if(scrollPos > 2700) { $('.fix .text').removeClass('on');}
 
 
     }
+
+    function fixHeader() {
+        if(scrollPos > 50) {$('header').addClass('on');}
+        else {$('header').removeClass('on');}
+    }
+
+    function fixList() {
+        $('section.approach .inner .list li a').removeClass('on');
+        if(scrollPos > 1250) {
+            $('section.approach .inner .list li a').removeClass('on');
+            $('section.approach .inner .list li:eq(0) a').addClass('on');
+        }
+
+        if(scrollPos > 1650) {
+            $('section.approach .inner .list li a').removeClass('on');
+            $('section.approach .inner .list li:eq(1) a').addClass('on');
+        }
+
+        if(scrollPos > 2050) {
+            $('section.approach .inner .list li a').removeClass('on');
+            $('section.approach .inner .list li:eq(2) a').addClass('on');
+        }
+
+        if(scrollPos > 2900) {
+            $('section.approach .inner .list li a').removeClass('on');
+            $('section.approach .inner .list li:eq(3) a').addClass('on');
+        }
+    }
 });
+
+/*스크롤 이벤트*/
+
+$(function() {
+    $('.animate').scrolla({
+        mobile: true,
+        once:false
+    })
+})
